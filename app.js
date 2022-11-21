9fb3a3cf4774d3d38f90d788518833da8a177bf4
mehmet <tas349873@gmail.com>
2022-11-21 22:38:17 +0300

    1212

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const arrows =document.querySelectorAll(".arrow");
+const movieLists =document.querySelectorAll(".movie-list");
+console.log(movieLists);
 
 
-
-arrows.forEach((arrow)=>{
+arrows.forEach((arrow,i)=>{
     arrow.addEventListener("click",function(){
-        
-    })
+       movieLists[i].style.transform=`translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value-300}px)`
+    });
 })
