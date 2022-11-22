67b9fe1d47eb820d3974e564b3998c8870199c99
mehmet <tas349873@gmail.com>
2022-11-22 22:39:48 +0300

    updt

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const arrows =document.querySelectorAll(".arrow");
-const movieLists =document.querySelectorAll(".movie-list");
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 console.log(movieLists);
 
+arrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+ 
+  if (imageItem - (4 + clickCounter) >= 0) {
+    arrow.addEventListener("click", function () {
+      clickCounter++;
 
-arrows.forEach((arrow,i)=>{
-    let clickCounter = 0;
-    const imageItem = movieLists[i].querySelectorAll("img").length;
-    console.log(imageItem);
-    if(imageItem-(4+clickCounter))
-    arrow.addEventListener("click",function(){
-        clickCounter++;
-        
-       movieLists[i].style.transform=`translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value-300}px)`
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
     });
-})
-
+  }else{
+    movieLists[i].style.transform="translateX(0)"
+  }
+});
