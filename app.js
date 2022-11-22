14e96dcf78cfdb1622bfd8638d5b2057d2a3976d
mehmet <tas349873@gmail.com>
2022-11-22 22:35:20 +0300

    js

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(movieLists);
 
 
 arrows.forEach((arrow,i)=>{
+    let clickCounter = 0;
+    const imageItem = movieLists[i].querySelectorAll("img").length;
+    console.log(imageItem);
+    if(imageItem-(4+clickCounter))
     arrow.addEventListener("click",function(){
+        clickCounter++;
+        
        movieLists[i].style.transform=`translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value-300}px)`
     });
 })
+
